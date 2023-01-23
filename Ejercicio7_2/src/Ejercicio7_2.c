3d4d81534fb23ca82770b34bf9bc03a044f51f35
Bianca <97748282+biancadecima@users.noreply.github.com>
2023-01-23 20:41:42 -0300

    Add files via upload

---
+++ Ejercicio7_2/src/Ejercicio7_2.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+Ejercicio 7-2: Dados 3 vectores paralelos de tipo entero:
+Cargar el vector A y B de manera secuencial.
+El tercer vector C, deberá cargarse con el mayor de los elementos del mismo componente.
+Mostrar los 3 vectores, ordenados de menor a mayor por el vector C
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
